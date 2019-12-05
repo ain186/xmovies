0b75822d2ab3b5a35e5d49aad3d075e9b50fdb5a
phuctd99 <thangtran990201@gmail.com>
2019-12-05 15:30:52 +0700

    fix and update

--- modules/backtotop.php
+++ modules/backtotop.php
@@ -14,4 +14,12 @@
             scrollTop: 0
         }, '300');
     });
+    function dropDown() {
+    var x = document.getElementById("Demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += "w3-show";
+    } else { 
+        x.className = x.className.replace("w3-show", "");
+    }
+    };
 </script>
